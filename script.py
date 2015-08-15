825b2d73f83ece4b6e789ce45a7595f2ebfdf7fd
Alex Badescu <badescuga@gmail.com>
2015-08-15 20:59:31 +0300

    working on it..
    the script works, problem though with the subliminal & apple automation
    services

--- script.py
+++ script.py
@@ -4,8 +4,20 @@ from __future__ import unicode_literals
 import os
 from babelfish import *
 from subliminal import *
+import urlparse
+import os.path
+import sys
+import xmlrpc_client
 
-video = Video.fromname('The.Big.Bang.Theory.S05E18.HDTV.x264-LOL.mp4')
+print len(sys.argv)
+if len(sys.argv) > 1:
+	moviePath = sys.argv[1]
+else:
+	moviePath = "/Users/badescuga/Downloads/Rick.and.Morty.S02E02.HDTV.x264-BATV.mp4"
+print "eeee"
+
+movieName = os.path.basename(urlparse.urlsplit(moviePath).path)
+video = Video.fromname(movieName)
 print video
 print video.video_codec
 print video.release_group
@@ -15,4 +27,17 @@ my_region = region.configure('dogpile.cache.memory')
 best_subtitles = download_best_subtitles([video], {Language('eng')}, providers=['podnapisi'])
 best_subtitles[video]
 best_subtitle = best_subtitles[video][0]
-print best_subtitle.content.split(b'\n')[2]
+
+#save subtitle
+
+subPath = os.path.splitext(moviePath)[0] + ".srt"
+print subPath
+file_ = open(subPath, 'w')
+file_.write(best_subtitle.content)
+file_.close()
+
+
+#print best_subtitle.content.split(b'\n')[2]
+
+#os.system("alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'")
+os.system("/Applications/VLC.app/Contents/MacOS/VLC " + moviePath + " --sub-file "+subPath)
