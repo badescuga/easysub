96b89078fc614acfe7d2244a1038c0d1a71afcd4
Alex Badescu <badescuga@gmail.com>
2015-08-22 23:35:51 +0300

    pushing subs to a /subs/ folder. makes data more organised and helps with tv shows scenarios.

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ import urlparse
 import os.path
 import sys
 
-print len(sys.argv)
+#print len(sys.argv)
 
 if len(sys.argv) > 1:
 	moviePath = sys.argv[1]
@@ -21,21 +21,33 @@ else:
 movieName = os.path.basename(urlparse.urlsplit(moviePath).path)
 video = Video.fromname(movieName)
 print video
-print video.video_codec
-print video.release_group
+#print video.video_codec
+#print video.release_group
 
 my_region = region.configure('dogpile.cache.memory')
 
+print 'started downloading subs..'
 best_subtitles = download_best_subtitles([video], {Language('eng')}, providers=None)
-print best_subtitles[video]
-print len(best_subtitles) 
-print len(best_subtitles[video])
+#print best_subtitles[video]
+print 'number of best subtitles: ' + str(len(best_subtitles)) 
+#print len(best_subtitles[video])
 best_subtitle = best_subtitles[video][0]
 
 #save subtitle
 
-subPath = os.path.splitext(moviePath)[0] + ".srt"
+#subPath = os.path.splitext(moviePath)[0] + ".srt"
+
+parentFolderPath = os.path.abspath(os.path.join(moviePath, os.pardir))
+
+##check if /subs/ dir exists and create it if not
+subsFolderPath = parentFolderPath+'/subs/'
+if not os.path.exists(subsFolderPath):
+    os.makedirs(subsFolderPath)
+
+subPath = subsFolderPath+movieName + '.srt'
+
 print subPath
+
 file_ = open(subPath, 'w')
 file_.write(best_subtitle.content)
 file_.close()
@@ -44,4 +56,8 @@ file_.close()
 #print best_subtitle.content.split(b'\n')[2]
 
 #os.system("alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'")
-os.system("/Applications/VLC.app/Contents/MacOS/VLC " + moviePath + " --sub-file "+subPath)
+os.system("/Applications/VLC.app/Contents/MacOS/VLC " + moviePath + " --sub-file "+subPath +" &")
+print 'exiting..'
+exit(0)
+
+
